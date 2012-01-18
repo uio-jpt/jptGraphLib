0323e6e555b9711b563fc668e176d0007a73840e
Eyvind W. Axelsen <eyvindwa@gmail.com>
2012-01-18 14:58:07 +0100

    Lagt til lisens-info.

--- src/AuxiliaryConceptImplementations.java
+++ src/AuxiliaryConceptImplementations.java
@@ -1,6 +1,31 @@
-/* Denne template'en inneholder to implementasjoner av Visitor-interfacet.
+/* Copyright 2012 © The SWAT project, OMS Group, Department of Informatics, 
+   University of Oslo.
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   -----------------------------------------------------------------------
+   
+   This product includes a modified version of software developed 
+   by the Open Systems Laboratory at Indiana University.  For 
+   further information contact Andrew Lumsdaine at lums@osl.iu.edu.
+
+   Copyright 2003 © The Trustees of Indiana University.  All rights
+   reserved.
+
+   See LICENSE_Indiana_University.txt for the full license for the
+   original code.
 */
+
 
 template AuxiliaryConceptImplementations {
 	
--- src/GraphConcepts.java
+++ src/GraphConcepts.java
@@ -1,10 +1,30 @@
-/* Template'en GraphConcepts inneholder hovedkonseptene for en graf, 
-   slik den er spesifisert i det opprinnelige biblioteket. Disse konseptene
-   blir i Java realsert som interfaces.  
+/* Copyright 2012 © The SWAT project, OMS Group, Department of Informatics, 
+   University of Oslo.
 
-  Implementasjoner finnes i template'en GraphConceptImplementations.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   -----------------------------------------------------------------------
+   
+   This product includes a modified version of software developed 
+   by the Open Systems Laboratory at Indiana University.  For 
+   further information contact Andrew Lumsdaine at lums@osl.iu.edu.
+
+   Copyright 2003 © The Trustees of Indiana University.  All rights
+   reserved.
+
+   See LICENSE_Indiana_University.txt for the full license for the
+   original code.
 */
-
 
 template GraphConcepts {
 	
